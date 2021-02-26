62858e6dfd1efab48c45a3ef5ffe9cb68c5efda8
Yegor Garmash <yegor_garmash@epam.com>
2021-02-26 20:51:45 +0200

    show property value on view

--- src/main/kotlin/com/cloud/training/functions/Properties.kt
+++ src/main/kotlin/com/cloud/training/functions/Properties.kt
@@ -1,2 +1,14 @@
 package com.cloud.training.functions
 
+import org.springframework.util.ResourceUtils
+import java.io.FileInputStream
+import java.util.Properties
+
+val file = ResourceUtils.getFile("classpath:view/messages/messages.properties")
+val properties: Properties = Properties().also {
+    it.load(FileInputStream(file))
+}
+
+fun getProp(key: String): String {
+    return properties[key] as String
+}
